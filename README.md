882447192a1a74cb5651a40889ee2bf6fbcb647c
Shugo Kawamura <ruk1ium@gmail.com>
2024-05-17 11:03:41 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dathost-go
 Go Unofficial API Library for dathost CS2 servers
+
+https://dathost.readme.io/reference/get_game_servers
---
+++ dathost.go
@@ -0,0 +1,11 @@
+package dathost
+
+// datHostClientv01 Client API v0.1
+type datHostClientv01 struct {
+	auth string // Basic auth with Base64-encoded username/password pair
+}
+
+// DatHostClientv01 Client API v0.1
+type DatHostClientv01 interface {
+	
+}

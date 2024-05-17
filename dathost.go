e8ae4150c9cc24972488d4af2d6747b3b2f55261
xxvw <tokuhara@gamers-buddy.com>
2024-05-17 14:24:41 +0900

    add request function

--- dathost.go
+++ dathost.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 )
 
+const baseURL = "https://dathost.net/api/0.1"
+
 // dathostClientv01 Client API v0.1
 type dathostClientv01 struct {
 	auth string // Basic auth with Base64-encoded username/password pair
@@ -14,11 +16,20 @@ type dathostClientv01 struct {
 
 // ListGameServers implements DatHostClientv01.
 func (dc *dathostClientv01) ListGameServers() {
-	ep := "https://dathost.net/api/0.1/game-servers"
-	req, err := http.NewRequest("GET", ep, nil)
+	ep := baseURL + "/game-servers"
+	body, err := dc.sendRequest("GET", ep, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
+	}
+	fmt.Println(string(body))
+}
+
+// sendRequest sends an HTTP request to the given endpoint with the specified method and body.
+func (dc *dathostClientv01) sendRequest(method, endpoint string, body io.Reader) ([]byte, error) {
+	req, err := http.NewRequest(method, endpoint, body)
+	if err != nil {
+		return nil, err
 	}
 
 	req.Header.Add("accept", "application/json")
@@ -26,16 +37,13 @@ func (dc *dathostClientv01) ListGameServers() {
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 	defer res.Body.Close()
-	body, _ := io.ReadAll(res.Body)
-
-	fmt.Println(string(body))
-
+	return io.ReadAll(res.Body)
 }
 
+// NewDathostClientv01 creates a new instance of DatHostClientv01.
 func NewDathostClientv01(username, password string) DatHostClientv01 {
 	base64Auth := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
 	return &dathostClientv01{
